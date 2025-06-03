1b980410f5015c1fa9bdd8acd2175b6a0a117911
Nuno Gaspar <88188118+yMisterBoss@users.noreply.github.com>
2025-06-03 21:03:54 +0100

    UPDATES

--- src/main/kotlin/routes/EstacionamentoRoutes.kt
+++ src/main/kotlin/routes/EstacionamentoRoutes.kt
@@ -9,22 +9,39 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
+
+data class EstacionamentoDTO(val localizacao: String, val status: String)
 
 fun Route.estacionamentoRoutes() {
+
     route("/api/estacionamentos") {
+
         get {
-            val ests = transaction {
+            val lista = transaction {
                 Estacionamento.selectAll().map {
-                    EstacionamentoDTO(it[Estacionamento.idEstacionamento], it[Estacionamento.localizacao], it[Estacionamento.status])
+                    EstacionamentoDTO(
+                        id = it[Estacionamento.idEstacionamento],
+                        localizacao = it[Estacionamento.localizacao],
+                        status = it[Estacionamento.status]
+                    )
                 }
             }
-            call.respond(ests)
+            call.respond(lista)
         }
 
-        post {
-            val novo = call.receive<EstacionamentoDTO>()
-            transaction { inserirEstacionamento( novo.localizacao) }
-            call.respond(HttpStatusCode.Created)
+        post("/update") {
+            val estados = call.receive<List<EstacionamentoDTO>>()
+
+            transaction {
+                estados.forEach { lugar ->
+                    Estacionamento.update({ Estacionamento.localizacao eq lugar.localizacao }) {
+                        it[status] = lugar.status
+                    }
+                }
+            }
+
+            call.respondText("Estado dos lugares atualizado com sucesso.")
         }
     }
 }
